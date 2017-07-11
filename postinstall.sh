6e2e1eae95518bbbcd8d5a313325b0d09e272705
RaEmber <raphael.emberger@hotmail.ch>
2017-07-11 10:03:30 +0200

    Extended script

--- postinstall.sh
+++ postinstall.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 # Install all needed packages
-sudo pacman -S vim firefox stow htop mlocate
+sudo pacman -S vim firefox stow mlocate wget
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-ma-zsh/master/tools/install.sh)"
 
 # Get dotfiles
-cd ~
-git clone https://github.com/RaEmber/manjaro-dotfiles.git dotfiles
-./dotfiles/install.sh
+git clone https://github.com/RaEmber/manjaro-dotfiles.git ~/dotfiles
+~/dotfiles/deletedefaults.sh
+~/dotfiles/install.sh
+
+# Setup git
+git config --global user.email "raphael.emberger@hotmail.ch"
+git config --global user.name "RaEmber"
+git config --system core.ignorecase false
