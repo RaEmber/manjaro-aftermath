482af903beb9212d48f0909921ebd6f2e62f1bbb
RaEmber <raphael.emberger@hotmail.ch>
2017-07-12 18:01:09 +0200

    Fixed script

--- postinstall.sh
+++ postinstall.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Install all needed packages
-sudo pacman -S vim firefox stow mlocate wget
+sudo pacman -S make fakeroot vim stow mlocate wget firefox
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Get dotfiles
-git clone https://RaEmber@github.com/RaEmber/manjaro-dotfiles.git ~/dotfiles
+git clone https://raember@github.com/raember/manjaro-dotfiles.git ~/dotfiles
 cd ~/dotfiles/
-./deletedefaults.sh
+./delete_defaults.sh
 ./install.sh
 
 # Setup git
 git config --global user.email "raphael.emberger@hotmail.ch"
-git config --global user.name "RaEmber"
-git config --system core.ignorecase false
+git config --global user.name "raember"
+sudo git config --system core.ignorecase false
